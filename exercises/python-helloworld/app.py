91e2ae694c1f67022b6bcf55e6b8a2654d0c4169
vu-minh <38859995+vu-minh@users.noreply.github.com>
2021-07-08 14:51:05 +0000

    add logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import json
+import logging
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ def hello():
 
 @app.route("/metrics")
 def show_metrics():
+    app.logger.info("Show metrics")
     return {"data": {"UserCount": 140, "UserCountActive": 23}}
 
 
@@ -22,8 +24,10 @@ def show_status():
         status=200,
         mimetype="application/json",
     )
+    app.logger.info("Show status")
     return response
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="app.log", level=logging.DEBUG)
     app.run(host="0.0.0.0")
