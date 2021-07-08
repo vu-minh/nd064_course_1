3b7eabaf8ca2c63aa8426638a0e77120d79212b0
vu-minh <38859995+vu-minh@users.noreply.github.com>
2021-07-08 14:39:00 +0000

    Excercie 1 helloworld

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,29 @@
 from flask import Flask
+import json
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! Udacity course!"
+
+
+@app.route("/metrics")
+def show_metrics():
+    return {"data": {"UserCount": 140, "UserCountActive": 23}}
+
+
+@app.route("/status")
+def show_status():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype="application/json",
+    )
+    return response
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
